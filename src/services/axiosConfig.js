95b035d13a7435b628818c1c89f80c580b6b5ebf
AvilesDanie <daaviles3@espe.edu.ec>
2025-02-12 08:38:49 -0500

    a

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosConfig = axios.create({
   baseURL: 'https://backlec-production.up.railway.app/api', // Configura la URL base aquí
-  timeout: 1000,
+  timeout: 10000000,
 });
 
 export default axiosConfig;
